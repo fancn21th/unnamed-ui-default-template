ae52441efcfa7c6a1b2d8942e6f446f1ed4d51bf
fhy429 <fhy429@126.com>
2025-12-30 14:36:48 +0800

    🐛 FIX: 修复build 警告问题

--- components/assistant-ui/thread/primitives/action-bar-extend/ActionBarLikeIf.tsx
+++ components/assistant-ui/thread/primitives/action-bar-extend/ActionBarLikeIf.tsx
@@ -1,12 +1,7 @@
 import {
-  ComponentPropsWithoutRef,
-  type ComponentRef,
   FC,
-  forwardRef,
   type PropsWithChildren,
 } from "react";
-import { Primitive } from "@radix-ui/react-primitive";
-import { composeEventHandlers } from "@radix-ui/primitive";
 import { useSmartVisionActionActions } from "@/runtime/smartVisionActionRuntime";
 
 export type Props = PropsWithChildren<{ like?: boolean; dislike?: boolean }>;
